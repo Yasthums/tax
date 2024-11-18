27a9851608b4da31d1aa0b5057009263bcc746d2
Yasthums <166440202+Yasthums@users.noreply.github.com>
2024-11-18 10:15:13 -0300

    Update leitor de xml.py

--- leitor de xml.py
+++ leitor de xml.py
@@ -1,79 +1,77 @@
-import os
-import xmltodict
-import pandas as pd
-
 # Pasta onde estão os arquivos XML
 pasta_xml = r'C:\Users\yasmin.thums\Desktop\Leitor de xml\XMLs'
 
-# Lista para armazenar os dados de todos os XMLs
-dados = []
+# Listas para armazenar os dados de todos os XMLs
+dados_nfse = []
+dados_nfe = []
+dados_cte = []
+
+def extract_elements(d, parent_key='', sep='_'):
+    """ Extrai todos os elementos de forma recursiva do dicionário XML """
+    items = {}
+    if isinstance(d, dict):
+        for k, v in d.items():
+            new_key = f"{parent_key}{sep}{k}" if parent_key else k
+            items.update(extract_elements(v, new_key, sep))
+    elif isinstance(d, list):
+        for i, v in enumerate(d):
+            new_key = f"{parent_key}{sep}{i}" if parent_key else str(i)
+            items.update(extract_elements(v, new_key, sep))
+    else:
+        items[parent_key] = d
+    return items
 
 # Itera sobre os arquivos na pasta
 for filename in os.listdir(pasta_xml):
     if filename.endswith('.xml'):
+        print(filename)
         file_path = os.path.join(pasta_xml, filename)
         with open(file_path, 'r', encoding='utf-8') as file:
             xml_content = file.read()
             obj = xmltodict.parse(xml_content)
-            print(obj)
-            try:
-                nfse_info = obj['ConsultarNfseResposta']['ListaNfse']['CompNfse']['Nfse']['InfNfse']
-                data = {
-                    'codigo_verificacao': nfse_info.get('CodigoVerificacao', ''),
-                    'competencia': nfse_info.get('Competencia', ''),
-                    'data_emissao': nfse_info.get('DataEmissao', ''),
-                    'incentivador_cultural': nfse_info.get('IncentivadorCultural', ''),
-                    'natureza_operacao': nfse_info.get('NaturezaOperacao', ''),
-                    'numero': nfse_info.get('Numero', ''),
-                    'optante_simples_nacional': nfse_info.get('OptanteSimplesNacional', ''),
-                    'orgao_gerador_codigo_municipio': nfse_info.get('OrgaoGerador', {}).get('CodigoMunicipio', ''),
-                    'orgao_gerador_uf': nfse_info.get('OrgaoGerador', {}).get('Uf', ''),
-                    'prestador_contato_email': nfse_info.get('PrestadorServico', {}).get('Contato', {}).get('Email', ''),
-                    'prestador_contato_telefone': nfse_info.get('PrestadorServico', {}).get('Contato', {}).get('Telefone', ''),
-                    'prestador_endereco_bairro': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Bairro', ''),
-                    'prestador_endereco_cep': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Cep', ''),
-                    'prestador_endereco_codigo_municipio': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('CodigoMunicipio', ''),
-                    'prestador_endereco_complemento': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Complemento', ''),
-                    'prestador_endereco_endereco': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Endereco', ''),
-                    'prestador_endereco_numero': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Numero', ''),
-                    'prestador_endereco_uf': nfse_info.get('PrestadorServico', {}).get('Endereco', {}).get('Uf', ''),
-                    'prestador_cnpj': nfse_info.get('PrestadorServico', {}).get('IdentificacaoPrestador', {}).get('Cnpj', ''),
-                    'prestador_inscricao_municipal': nfse_info.get('PrestadorServico', {}).get('IdentificacaoPrestador', {}).get('InscricaoMunicipal', ''),
-                    'prestador_razao_social': nfse_info.get('PrestadorServico', {}).get('RazaoSocial', ''),
-                    'servico_codigo_municipio': nfse_info.get('Servico', {}).get('CodigoMunicipio', ''),
-                    'servico_codigo_tributacao_municipio': nfse_info.get('Servico', {}).get('CodigoTributacaoMunicipio', ''),
-                    'servico_discriminacao': nfse_info.get('Servico', {}).get('Discriminacao', ''),
-                    'servico_item_lista_servico': nfse_info.get('Servico', {}).get('ItemListaServico', ''),
-                    'servico_valores_aliquota': nfse_info.get('Servico', {}).get('Valores', {}).get('Aliquota', ''),
-                    'servico_valores_base_calculo': nfse_info.get('Servico', {}).get('Valores', {}).get('BaseCalculo', ''),
-                    'servico_valores_iss_retido': nfse_info.get('Servico', {}).get('Valores', {}).get('IssRetido', ''),
-                    'servico_valores_inss': nfse_info.get('Servico', {}).get('Valores', {}).get('ValorInss', ''),
-                    'servico_valores_iss': nfse_info.get('Servico', {}).get('Valores', {}).get('ValorIss', ''),
-                    'servico_valores_liquido_nfse': nfse_info.get('Servico', {}).get('Valores', {}).get('ValorLiquidoNfse', ''),
-                    'servico_valores_servicos': nfse_info.get('Servico', {}).get('Valores', {}).get('ValorServicos', ''),
-                    'tomador_contato_email': nfse_info.get('TomadorServico', {}).get('Contato', {}).get('Email', ''),
-                    'tomador_contato_telefone': nfse_info.get('TomadorServico', {}).get('Contato', {}).get('Telefone', ''),
-                    'tomador_endereco_bairro': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Bairro', ''),
-                    'tomador_endereco_cep': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Cep', ''),
-                    'tomador_endereco_codigo_municipio': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('CodigoMunicipio', ''),
-                    'tomador_endereco_complemento': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Complemento', ''),
-                    'tomador_endereco_endereco': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Endereco', ''),
-                    'tomador_endereco_numero': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Numero', ''),
-                    'tomador_endereco_uf': nfse_info.get('TomadorServico', {}).get('Endereco', {}).get('Uf', ''),
-                    'tomador_cnpj': nfse_info.get('TomadorServico', {}).get('IdentificacaoTomador', {}).get('CpfCnpj', {}).get('Cnpj', ''),
-                    'tomador_inscricao_municipal': nfse_info.get('TomadorServico', {}).get('IdentificacaoTomador', {}).get('InscricaoMunicipal', ''),
-                    'tomador_razao_social': nfse_info.get('TomadorServico', {}).get('RazaoSocial', '')
-                }
-                dados.append(data)
-            except KeyError as e:
-                print(f"Chave não encontrada em {filename}: {e}")
-                continue
 
-# Criar um DataFrame pandas com os dados coletados
-df = pd.DataFrame(dados)
+            # Verifica se é NFse
+            if 'ConsultarNfseResposta' in obj:
+                try:
+                    nfse_info = obj['ConsultarNfseResposta']['ListaNfse']['CompNfse']['Nfse']['InfNfse']
+                    # Extrai dados da NFse e adiciona à lista
+                    data_nfse = extract_elements(nfse_info)
+                    dados_nfse.append(data_nfse)
+                    print(f"Dados NFse encontrados em {filename}")
+                except KeyError as e:
+                    print(f"Chave NFse não encontrada em {filename}: {e}")
 
-# Exportar o DataFrame para um arquivo Excel
-excel_file = r'C:\Users\yasmin.thums\Desktop\Leitor de xml\dados_nfse.xlsx'
-df.to_excel(excel_file, index=False)
+            # Verifica se é NFe
+            if 'nfeProc' in obj:
+                try:
+                    nfe_info = obj['nfeProc']['NFe']['infNFe']
+                    # Extrai dados da NFe e adiciona à lista
+                    data_nfe = extract_elements(nfe_info)
+                    dados_nfe.append(data_nfe)
+                    print(f"Dados NFe encontrados em {filename}")
+                except KeyError as e:
+                    print(f"Chave NFe não encontrada em {filename}: {e}")
 
-print(f'Dados exportados para {excel_file}')
+            # Verifica se é CTe
+            if 'cteProc' in obj:
+                try:
+                    cte_info = obj['cteProc']['CTe']['infCte']
+                    # Extrai dados do CTe e adiciona à lista
+                    data_cte = extract_elements(cte_info)
+                    dados_cte.append(data_cte)
+                    print(f"Dados CTe encontrados em {filename}")
+                except KeyError as e:
+                    print(f"Chave CTe não encontrada em {filename}: {e}")
+
+# Convertendo os dados para DataFrames
+df_nfse = pd.DataFrame(dados_nfse)
+df_nfe = pd.DataFrame(dados_nfe)
+df_cte = pd.DataFrame(dados_cte)
+
+# Exportando para Excel
+with pd.ExcelWriter('dados_fiscais.xlsx') as writer:
+    df_nfse.to_excel(writer, sheet_name='NFse', index=False)
+    df_nfe.to_excel(writer, sheet_name='NFe', index=False)
+    df_cte.to_excel(writer, sheet_name='CTe', index=False)
+
+print("Dados exportados para Excel com sucesso!")
